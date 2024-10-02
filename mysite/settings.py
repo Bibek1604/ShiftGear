7affce40f2973123ccbf77d0766622e71a20c116
Bibek1604 <npbibek94@gmail.com>
2024-10-02 21:36:20 +0545

    secret key update on setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,6 +17,11 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -26,7 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+import secrets
+print(secrets.token_urlsafe(50))
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
