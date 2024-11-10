3994cbc59f7141e3958acebb502c4df535da81f1
Bibek1604 <npbibek94@gmail.com>
2024-11-10 19:33:22 +0545

    Make build.sh executable

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
-set -o errexit
+set -o errexit  # Exit on error
 
-# Run the tests
-pip install  -r requirements.txt
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Apply database migrations
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
+
+# Create a superuser if environment variables are set
+if [[ "$CREATE_SUPERUSER" == "true" ]]; then
     python manage.py createsuperuser --noinput --email "$DJANGO_SUPERUSER_EMAIL"
 fi
